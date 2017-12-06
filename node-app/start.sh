5cbe2c06b67e0fd51b9d9920df05896acc74b75f
Nathan Arnold <natearn@natearn.info>
2017-12-06 20:30:33 +0100

    update node-app with automatic build condition

--- node-app/start.sh
+++ node-app/start.sh
@@ -5,6 +5,10 @@ IMAGE_NAME="$1"
 CONTAINER="$2"
 COMMAND="sh"
 
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]];
+then docker build -t $IMAGE_NAME .
+fi
+
 if docker inspect -f '{{.State.Running}}' "$CONTAINER" &> /dev/null
 then docker exec -it "$CONTAINER" "$COMMAND"
 else docker run -it --rm --name "$CONTAINER" \
